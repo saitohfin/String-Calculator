4c0009c4d142d5588eef078a9f5a4b5266de5fb0
saitohfin <javidesoft@gmail.com>
2016-01-25 00:53:22 +0100

    fiveLessfourReturnOne passed

--- src/kata/tdd/stringcalculator/StringCalculator.java
+++ src/kata/tdd/stringcalculator/StringCalculator.java
@@ -8,9 +8,18 @@ public class StringCalculator {
             Integer summand1 = Integer.parseInt(summands[0]);
             Integer summand2 = Integer.parseInt(summands[1]);
             return Integer.toString(summand1 + summand2);
+        }else if(isAnASubstraction(value)){
+            String[] operands = value.split("\\-");
+            Integer operand1 = Integer.parseInt(operands[0]);
+            Integer operan2 = Integer.parseInt(operands[1]);
+            return Integer.toString(operand1 - operan2);
         }
         //Y esto va empezando a coger una pinta de patrón estrategia que pa qué...
         return value;
+    }
+
+    private boolean isAnASubstraction(String value) {
+        return value.contains("-");
     }
 
     private boolean isAnAddition(String value) {
